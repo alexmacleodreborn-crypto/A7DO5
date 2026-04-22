be30dc15d1f23728334d8f2080b8d6ee838bea42
alexmacleodreborn-crypto <alexmacleodreborn@gmail.com>
2026-04-22 11:18:54 +0100

    Update

--- __init__.py
+++ __init__.py
@@ -1,21 +1,3 @@
-# A7DO Biomechanical Engine
-from .core.a7do_engine import A7DOEngine, create_a7do
-from .data.anatomy_database import (
-    BONES, MUSCLES, NERVES, BLOOD_VESSELS, ORGANS,
-    LIGAMENTS_TENDONS, ENDOCRINE, LYMPHATIC, GROWTH_TIMELINE
-)
+from .a7do_engine import A7DOEngine, create_a7do
 
-__version__ = "1.0.0"
-__all__ = [
-    'A7DOEngine',
-    'create_a7do',
-    'BONES',
-    'MUSCLES',
-    'NERVES',
-    'BLOOD_VESSELS',
-    'ORGANS',
-    'LIGAMENTS_TENDONS',
-    'ENDOCRINE',
-    'LYMPHATIC',
-    'GROWTH_TIMELINE'
-]
+__all__ = ['A7DOEngine', 'create_a7do']
